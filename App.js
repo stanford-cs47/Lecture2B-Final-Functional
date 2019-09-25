1d74d195b12d44b82cb3230e78daff498605c3cd
Tiffany Manuel <TiffanyManuel@DNa1d2589.SUNet>
2019-09-24 18:29:27 -0700

    first commit

--- App.js
+++ App.js
@@ -1,46 +1,97 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, ActivityIndicator } from 'react-native';
 import { Images, Colors, Metrics } from './App/Themes';
+import StarWarsCard from './App/Components/StarWarsCard'
 
+/*
+  Displays information about Jedi
+*/
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    // see what props App.js is constructed with:
+    console.log(JSON.stringify(props));
+  }
+
+  // this is our default state
+  state = {
+    jedi: {
+      name: 'Unknown',
+      gender: 'Unknown',
+      birthYear: 'N/A',
+      height: 'N/A',
+      weight: 'N/A',
+      hairColor: 'N/A',
+      eyeColor: 'N/A',
+      skinColor: 'N/A',
+    },
+    buttonText: 'Show me your ID Card!',
+    loading: false,
+  }
+
+  showCard = () => {
+    this.getJedi();
+
+    // Example of how to change state
+    // let newJedi = { name: 'Darth Vader', gender: 'Male', birthYear: '41.9BBY', height: '202cm', weight: '136kg', eyeColor: 'yellow', hairColor: 'none', skinColor: 'white' };
+    // this.setState({jedi: newJedi}); //never change state directly, always use this
+  }
+
+  // async/await - ES6
+  // API that provides randomly generated people
+  async getJedi() {
+    try {
+
+      this.setState({loading: true});
+
+      let randomNumber = Math.floor(Math.random() * 88) + 1;
+      let response = await fetch(`https://swapi.co/api/people/${randomNumber}`);
+      let responseJson = await response.json();
+
+      // console.log(responseJson);
+
+      // Cleaning up our response...
+      let cleanObject = responseJson;
+      cleanObject.hairColor = responseJson.hair_color;
+      cleanObject.skinColor = responseJson.skin_color;
+      cleanObject.eyeColor = responseJson.eye_color;
+      cleanObject.birthYear = responseJson.birth_year;
+      cleanObject.weight = responseJson.mass;
+
+      this.setState({loading: false, jedi: cleanObject})
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   render() {
+
+    // Conditional rendering
+    if(this.state.loading) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
     return (
       <View style={styles.container}>
-        <View style={styles.card}>
 
-          <View style={styles.pictureView}>
-            <Image style={styles.picture}
-            source={Images.jedi1}/>
+         <StarWarsCard jedi={this.state.jedi} />
 
-            <View style={styles.pictureDetails}>
-              <Text style={ { fontWeight: 'bold' } }>Luke Skywalker</Text>
-              <Text style={ { fontWeight: '500' } }>Male</Text>
-            </View>
-          </View>
+         <Button
+          title={this.state.buttonText}
+          onPress={this.showCard}
+         />
 
-          <View style={styles.jediRowItem}>
-            <Text style={ { fontWeight: 'bold' } }>Birth Year</Text>
-            <Text style={ { fontWeight: 'bold' } }>Height</Text>
-            <Text style={ { fontWeight: 'bold' } }>Weight</Text>
-          </View>
-          <View style={[styles.jediRowItem, { marginTop: 0 }]}>
-            <Text>19</Text>
-            <Text>1.72</Text>
-            <Text>77</Text>
-          </View>
-
-          <View style={styles.jediRowItem}>
-            <Text style={ { fontWeight: 'bold' } }>Hair Color</Text>
-            <Text style={ { fontWeight: 'bold' } }>Eye Color</Text>
-          </View>
-          <View style={[styles.jediRowItem, { marginTop: 0 }]}>
-            <Text>Brown</Text>
-            <Text>Blue</Text>
-          </View>
-        </View>
       </View>
     );
+
   }
+
 }
 
 const styles = StyleSheet.create({
@@ -49,34 +100,5 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.snow,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  card: {
-    padding: Metrics.doubleBaseMargin,
-    width: Metrics.screenWidth * .9,
-    borderWidth: Metrics.borderWidth,
-    borderRadius: Metrics.buttonRadius
-  },
-  pictureView: {
-    marginLeft: Metrics.marginHorizontal,
-    marginRight: Metrics.marginHorizontal,
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'center'
-  },
-  picture: {
-    height: Metrics.images.large,
-    width: Metrics.images.large,
-    borderRadius: Metrics.images.large * 0.5
-  },
-  pictureDetails: {
-    flexDirection: 'column',
-    marginLeft: Metrics.marginHorizontal,
-    marginRight: Metrics.marginHorizontal,
-  },
-  jediRowItem: {
-    marginTop: Metrics.marginVertical,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center'
   }
 });
